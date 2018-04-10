d883e0251b2934fcd7133ffda632ae47bbf8044c
Avinesh Prasad <jessicaavinesh@gmail.com>
2018-04-10 10:53:36 -0700

    initial commit

---
+++ src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Landing = () => {
+return (
+  <div style={{textAlign: 'center'}}>
+<h1 class="blue-text text-darken-2">
+  Avi Surveys!
+</h1>
+    This is my survey app also my final for Coder Camps!
+   </div>
+
+);
+
+};
+
+export default Landing;
